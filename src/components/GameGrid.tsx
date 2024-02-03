6b7af6d47995b4e42c1a0321f46e3e9e5d759bee
Jownsu <jhonesdigno666@gmail.com>
2024-02-03 11:53:58 +0800

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -18,8 +18,8 @@ const GameGrid = () => {
 			{error && <Text>{error}</Text>}
 
 			<SimpleGrid
-				columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-				spacing="20px"
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				spacing={3}
 				padding="10px"
 			>
 				{isLoading &&
