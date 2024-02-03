1d9a6bd3096d20c0984dafd571d7e5aaf9f99688
Jownsu <jhonesdigno666@gmail.com>
2024-02-03 09:26:27 +0800

    Get optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,6 @@
+const getCroppedImageUrl = (url: string) => {
+    const temp_url = url.split("media/");
+    return `${temp_url[0]}media/crop/600/400/${temp_url[1]}`;
+};
+
+export default getCroppedImageUrl;
