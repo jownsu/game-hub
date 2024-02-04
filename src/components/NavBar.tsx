c4f9019bbd99973a8e49b7f567931c16d40e7b2d
Jownsu <jhonesdigno666@gmail.com>
2024-02-04 15:02:04 +0800

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,11 +6,13 @@ import ColorModeSwitch from "./ColorModeSwitch";
 
 /* ASSETS */
 import logo from "../assets/logo.webp";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
 	return (
-		<HStack justifyContent="space-between" padding="10px">
+		<HStack padding="10px">
 			<Image src={logo} boxSize={"60px"} />
+			<SearchInput />
 			<ColorModeSwitch />
 		</HStack>
 	);
