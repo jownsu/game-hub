bfb49f460845eed4131d70b67efde2ad0e524fbf
Jownsu <jhonesdigno666@gmail.com>
2024-02-04 12:47:00 +0800

    Refactor: Extract a query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,16 +7,15 @@ import GameCard from "./GameCard";
 /* COMPONENTS */
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import useGames, { Platform } from "../hooks/useGames";
-import { Genre } from "../hooks/useGenres";
+import useGames from "../hooks/useGames";
+import { GameQuery } from "../App";
 
 interface Props {
-	selectedGenre: Genre | null;
-	selectedPlatform: Platform | null;
+	gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-	const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+	const { data, error, isLoading } = useGames(gameQuery);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	return (
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 export interface Platform {
 	id: number;
 	name: string;
@@ -13,10 +13,11 @@ export interface Game {
 	metacritic: number;
 }
 
-const useGames = (genre: Genre | null, platform: Platform | null) =>
-	useData<Game>("games", { params: { genres: genre?.id, platforms: platform?.id } }, [
-		genre?.id,
-		platform?.id
-	]);
+const useGames = (gameQuery: GameQuery) =>
+	useData<Game>(
+		"games",
+		{ params: { genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id } },
+		[gameQuery]
+	);
 
 export default useGames;
