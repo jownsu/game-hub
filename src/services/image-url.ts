bda1490eec9628b03e4fc618f2c65db6b3cb591c
Jownsu <jhonesdigno666@gmail.com>
2024-02-04 13:34:45 +0800

    Added images and handled placeholder for games if no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-	if (!url) return "";
+	if (!url) return noImage;
 	const temp_url = url.split("media/");
 	return `${temp_url[0]}media/crop/600/400/${temp_url[1]}`;
 };
