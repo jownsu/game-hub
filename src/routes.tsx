634356eb354274d98b5af61104144dfbd8d8a74e
Jownsu <jhonesdigno666@gmail.com>
2024-02-15 14:18:53 +0800

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import GameDetails from "./pages/GameDetails";
 import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
+		errorElement: <ErrorPage />,
 		children: [
 			{
 				index: true,
